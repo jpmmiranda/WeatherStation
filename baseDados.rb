1957fcf2d1cec8fbfb352f698eb77a1593c5470c
jpmmiranda <jota_pmm@hotmail.com>
2017-03-06 17:06:07 +0000

    Alterei registos da BD para case sensitive

--- baseDados.rb
+++ baseDados.rb
@@ -11,25 +11,25 @@
     end
 
     def verificaClienteExiste(long,lat)
-        rs = @con.query("SELECT COUNT(*) from local where longitude= " + long + 
-          " and latitude= " + lat)
+        rs = @con.query("SELECT COUNT(*) from Local where Longitude= " + long + 
+          " and Latitude= " + lat)
        
         total = rs.fetch_row 
         if(total==0) 
-          rs = @con.query 'insert into local values ( ' + long  + ', ' + lat + ')' 
+          rs = @con.query 'insert into Local values ( ' + long  + ', ' + lat + ')' 
         end
     end
 
     def adicionaRegistoTemperatura(leitura,timestamp,long,lat)
 
-       rs = @con.query 'insert into registos (temperatura, data,local_longitude,local_latitude) values ( ' +   
+       rs = @con.query 'insert into Registos (Temperatura, Data,Local_Longitude,Local_Latitude) values ( ' +   
             leitura + ", '#{timestamp}'" + ', ' + long + ', ' + lat + ')' 
       
     end
 
 
     def adicionaRegistoRuido(leitura,timestamp,long,lat)
-      rs = @con.query 'insert into registos (ruído, data,local_longitude,local_latitude) values ( ' +   
+      rs = @con.query 'insert into Registos (Ruído, Data,Local_Longitude,Local_Latitude) values ( ' +   
             leitura + ", '#{timestamp}'" + ', ' + long + ', ' + lat + ')'
     end
   end
